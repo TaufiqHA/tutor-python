da9a27da3d935d84f9a0074757cc79197110c275
TaufiqHA <htaufiq225@gmail.com>
2022-10-09 21:52:24 +0700

    materi variabel

--- variable.py
+++ variable.py
@@ -3,4 +3,23 @@
 a = 5
 b = 7
 c = a + b
+
+# pemanggilan pertama
 print("Nilai dari c adalah", c)
+print("Nilai dari a adalah", a)
+print("Nilai dari b adalah", b)
+
+# penamaan
+nilai_y = 20  # benar
+# 10benar = 10 #salah
+benar10 = 10  # benar
+print("Nilai y adalah", nilai_y)
+print("Nilai benar10 adalah", benar10)
+
+# pemanggilan ke dua
+a = 1
+print("Nilai dari a adalah", a)
+
+# assignment indirect
+d = a
+print("Nilai dari d adalah", d)
